7a010fda03d644ecfa1e103d636819ae4088adee
chriscooler <apple@macde-MacBook-Pro.local>
2023-11-05 16:43:11 +0100

    delect svelte preprosse

--- frontend/src/components/UserProfileModal.svelte
+++ frontend/src/components/UserProfileModal.svelte
@@ -1,46 +1,32 @@
-<script>
-    import { chanel, chanelname, session } from "$lib/store/store";
+<script lang="ts">
+    import { session } from "$lib/store/store";
     import { onMount } from 'svelte';
-    export let username ; 
-    let user = null;
-    // let userNamee = usernale
-    let pictureLink;
+
+    export let username: any ; 
+    
+    let user: any = null;
+    let pictureLink: any;
 
 
     onMount(() => {
         console.log('here', username)
-            // Assume $session has an 'on' method to listen to events
             $session.emit('message', username);
 
-            $session.on("userResponse", (data) => {
+            $session.on("userResponse", (data: any) => {
             user = data.user;
             pictureLink = user.avatar;
 
             });
-
-            // $session.on('message', handleIncomingMessage);
-            
-            // // Send a request for user data
-            // $session.send(JSON.stringify({ action: 'getUser', username: username }));
-
-            // // Clean up the listener when the component is destroyed
             return () => {
                 $session.off('userResponse');
             };
-        
     });
 
-    // function handleIncomingMessage(event) {
-    //     const data = JSON.parse(event.data);
-    //     if (data.action === 'userResponse' && data.username === username) {
-    //         user = data.user;
-    //     }
-    // }
+
 </script>
 
-<!-- Display logic -->
+
 <div class="modal-content">
-    <!-- <h2>{username}'s Profile</h2> -->
 
     {#if user}
     <div>
@@ -54,9 +40,8 @@
         <p>Rank: {user.rank}</p>
         <p>Title: {user.title}</p>
         <p>Total Won: {user.wonGameNbr} - {user. lostGameNbr} :Lost</p>
-        <!-- ... other user data -->
     {:else}
         <p>Loading user data...</p>
     {/if}
-    <!-- <button on:click={() => isModalVisible = false}>Close</button> -->
+
 </div>
