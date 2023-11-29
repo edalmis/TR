720383b0e8bcccfe69070ebde4ac9ca4dc095b57
Balbeck <albecker.hector@gmail.com>
2023-11-29 23:12:00 +0100

    clean entrypoint + Dockerfile

--- frontend/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Read the FRONTEND_PORT value from .env
-FRONTEND_PORT=$(grep '^FRONTEND_PORT=' ../../.env | cut -d'=' -f2)
-
-# Update the Dockerfile with the exposed port
-sed -i "s/EXPOSE [0-9]*/EXPOSE $FRONTEND_PORT/" Dockerfile
